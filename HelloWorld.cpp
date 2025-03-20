720ce20cec2975393295eab2c504fe55de4a231f
Saxon Hays <81133539+SHays24@users.noreply.github.com>
2025-03-20 21:35:12 +1100

    first commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <cstddef>
+int main();
+using namespace std;
+typedef bool (*ScriptFunction)(char [], int (*)());
+unordered_map<string, ScriptFunction> commandsMap;
+bool test1(char args[], int (*callFunc)()) {
+    cout << "test2\n";
+
+    cout << args[0];
+    cout << args[1] << "\n";
+    return callFunc(); 
+}
+/*int testRunner(int (*callFunc)()) {
+    int a = 2;
+    int *pA = &a;
+    cout << *pA;
+    a++;
+    cout << *pA << "\n";
+    char arr[] = {'a', 'b'};
+    return commandsMap["test"](arr, callFunc);
+}*/
+int main() {
+    cout << "> ";
+    string rawCommand;
+    cin >> rawCommand;
+    string command = rawCommand.substr(0, rawCommand.find_first_of(' '));
+    commandsMap["test"] = &test1;
+    char arr[] = {'a', 'b'};
+    commandsMap[command](arr, &main);
+}
