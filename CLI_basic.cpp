c2f7da3ebc95430aa59e0cb73c5d6f2548946722
Saxon Hays <81133539+SHays24@users.noreply.github.com>
2025-03-22 14:16:09 +1100

    idk, still one uncaugth edge case, correct length arguments, but invalid type type situation

--- CLI_basic.cpp
+++ CLI_basic.cpp
@@ -4,33 +4,43 @@
 #include <cstddef>
 int main();
 using namespace std;
-typedef bool (*ScriptFunction)(char [], int (*)());
+typedef bool (*ScriptFunction)(string, int (*)());
 unordered_map<string, ScriptFunction> commandsMap;
-bool test1(char args[], int (*callFunc)()) {
+unordered_map<string, int> argsMap;
+bool test1(string args, int (*callFunc)()) {
+    char argsArr[2];
+    //cin >> args[0];
+    //cin >> args[1];
+    const int argsNum = 2;
+    //int iterator = 0;
+    char arr[argsNum];
+    //int argCnt = 0;
+    argsArr[0] = args.at(args.find_first_of(' ')+1);
+    argsArr[1] = args.at(args.find_first_of(' ', 1)+1);
     cout << "test2\n";
-
-    cout << args[0];
-    cout << args[1] << "\n";
-    return callFunc(); 
+    cout << argsArr[0];
+    cout << argsArr[1] << "\n";
+    return callFunc();
 }
-/*int testRunner(int (*callFunc)()) {
-    int a = 2;
-    int *pA = &a;
-    cout << *pA;
-    a++;
-    cout << *pA << "\n";
-    char arr[] = {'a', 'b'};
-    return commandsMap["test"](arr, callFunc);
-}*/
+
 int main() {
     cout << "> ";
     string rawCommand;
-    cin >> rawCommand;
-    string command = rawCommand.substr(0, rawCommand.find_first_of(' '));
-    commandsMap["test"] = &test1;
-    char arr[] = {'a', 'b'};
-    if (commandsMap.count(command) > 0) {
-        commandsMap[command](arr, &main);
+    getline(cin, rawCommand);
+    if (rawCommand.length() <= 1) {
+        return main();
     }
-    return main();
+    commandsMap["test"] = &test1;
+    argsMap["test"] = 2;
+    string command = rawCommand.substr(0, rawCommand.find_first_of(' '));
+    string args = "";
+    if (rawCommand.find_first_of(' ') <= rawCommand.length() && rawCommand.length() - command.length() > argsMap[command]) {
+        args = rawCommand.substr(rawCommand.find_first_of(' '));
+    } else {
+        return main();
+    }
+    if (commandsMap.count(command) > 0) {
+        commandsMap[command](args, &main);
+    }
+    //return main();
 }
